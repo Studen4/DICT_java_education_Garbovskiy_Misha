d9abbf5e4dee4c89a49023f93b554f4bc8213263
Studen4 <m.a.harbovskyi@student.khai.edu>
2023-12-18 23:49:45 +0200

    Matrix final-stage completed.

---
+++ src/MatrixProcessing/Main.java
@@ -0,0 +1,10 @@
+package MatrixProcessing;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MatrixVisual calculator = new MatrixVisual();
+        calculator.run();
+    }
+
+}
